4f7416674aa6a5374f19cf8c6ff9090170b4d4a6
Lars Kappert <lars@webpro.nl>
2013-06-20 20:07:47 +0200

    Yo dawg, I hear you don't like semi-colons, so I removed 'em

--- bin/html-inspector.js
+++ bin/html-inspector.js
@@ -2,24 +2,24 @@
 
 var fs = require('fs'),
   path = require('path'),
-  program = require('commander');
+  program = require('commander')
 
 var inspectLocation,
   baseVersions = ['full', 'core', 'convention', 'best-practices', 'validation'],
   baseVersion,
-  configFile;
+  configFile
 
 var basePath = path.normalize(__dirname + path.sep + '..'),
-  phantomRunner = basePath + path.sep + 'bin' + path.sep + 'phantom-runner.js';
+  phantomRunner = basePath + path.sep + 'bin' + path.sep + 'phantom-runner.js'
 
 program
   .version('0.0.1')
   .usage('[options] <file or url>')
   .option('-c, --config [file]', 'Configuration file')
   .option('-b, --base [base]', 'Use HTML Inspector version (' + baseVersions.join(', ') + ')', 'full')
-  .parse(process.argv);
+  .parse(process.argv)
 
-baseVersion = program.base;
+baseVersion = program.base
 
 if(program.args.length !== 1 || baseVersions.indexOf(baseVersion) === -1) {
   program.help()
@@ -27,29 +27,29 @@ if(program.args.length !== 1 || baseVersions.indexOf(baseVersion) === -1) {
 
 // Try to resolve local file, otherwise assume and pass url
 var inputLocation = program.args[0],
-  inspectLocation = fs.exists(path.resolve(inputLocation)) ? path.resolve(inputLocation) : inputLocation;
+  inspectLocation = fs.exists(path.resolve(inputLocation)) ? path.resolve(inputLocation) : inputLocation
 
 if(program.config) {
-  configFile = path.resolve(program.config);
+  configFile = path.resolve(program.config)
 }
 
 // Spawn process to run PhantomJS from CLI
 // TODO: Is this the way to go?
 function run(cmd, args, callback) {
-  var spawn = require('child_process').spawn;
-  var command = spawn(cmd, args);
-  var result = '';
+  var spawn = require('child_process').spawn
+  var command = spawn(cmd, args)
+  var result = ''
   command.stdout.on('data', function(data) {
-    result += data.toString();
-  });
+    result += data.toString()
+  })
   command.on('error', function() {
-    console.log(arguments);
-  });
+    console.log(arguments)
+  })
   command.on('close', function(code) {
-    return callback(result);
-  });
+    return callback(result)
+  })
 }
 
 run("phantomjs", [phantomRunner, basePath, baseVersion, inspectLocation, configFile], function(result) {
-  console.log(result);
-});
+  console.log(result)
+})
--- bin/phantom-runner.js
+++ bin/phantom-runner.js
@@ -2,7 +2,7 @@
 var basePath = phantom.args[0],
   baseVersion = phantom.args[1],
   inspectLocation = phantom.args[2],
-  configFile = phantom.args[3];
+  configFile = phantom.args[3]
 
 var injectVersions = {
   full: [''],
@@ -10,39 +10,39 @@ var injectVersions = {
   convention: ['.core', '.convention'],
   'best-practices': ['.core', '.best-practices'],
   validation: ['.core', '.validation']
-};
+}
 
-var page = require('webpage').create();
+var page = require('webpage').create()
 
 page.onConsoleMessage = function(msg) {
-  console.log(msg);
-};
+  console.log(msg)
+}
 
 page.onLoadFinished = function(status) {
 
   if(status !== 'success') {
-    console.log('Unable to open', location);
-    phantom.exit();
+    console.log('Unable to open', location)
+    phantom.exit()
   }
 
   var hasInspectorScript = page.evaluate(function() {
-    return 'HTMLInspector' in this;
-  });
+    return 'HTMLInspector' in this
+  })
 
   if(!hasInspectorScript) {
     injectVersions[baseVersion].forEach(function(version) {
-      page.injectJs(basePath + '/dist/html-inspector' + version + '.js');
-    });
+      page.injectJs(basePath + '/dist/html-inspector' + version + '.js')
+    })
   }
 
   if(configFile) {
-    page.injectJs(configFile);
+    page.injectJs(configFile)
   }
 
   page.evaluate(function() {
-    HTMLInspector.inspect();
-  });
-  phantom.exit();
-};
+    HTMLInspector.inspect()
+  })
+  phantom.exit()
+}
 
-page.open(inspectLocation);
+page.open(inspectLocation)
