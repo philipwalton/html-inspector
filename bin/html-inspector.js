cc405b5aa4c7cb10c6391d151b4f291c21dedc21
Lars Kappert <lars@webpro.nl>
2013-06-24 21:45:47 +0200

    Added simple console reporter

--- bin/html-inspector.js
+++ bin/html-inspector.js
@@ -7,21 +7,25 @@ var fs = require('fs'),
 var inspectLocation,
   baseVersions = ['full', 'core', 'convention', 'best-practices', 'validation'],
   baseVersion,
-  configFile
+  configFile,
+  reporters = ['console.simple'],
+  reporter;
 
 var basePath = path.normalize(__dirname + path.sep + '..'),
-  phantomRunner = basePath + path.sep + 'bin' + path.sep + 'phantom-runner.js'
+  phantomRunner = basePath + path.sep + 'bin' + path.sep + 'phantom-runner.js',
+  reporterBasePath = path.resolve(basePath + '/src/reporter') + path.sep
 
 program
   .version('0.0.1')
   .usage('[options] <file or url>')
   .option('-c, --config [file]', 'Configuration file')
   .option('-b, --base [base]', 'Use HTML Inspector version (' + baseVersions.join(', ') + ')', 'full')
+  .option('-r, --reporter [reporter]', 'Reporter (' + reporters.join(', ') + ')', 'console.simple')
   .parse(process.argv)
 
 baseVersion = program.base
 
-if(program.args.length !== 1 || baseVersions.indexOf(baseVersion) === -1) {
+if(program.args.length !== 1 || baseVersions.indexOf(baseVersion) === -1 || reporters.indexOf(program.reporter) === -1) {
   program.help()
 }
 
@@ -32,6 +36,8 @@ var inputLocation = program.args[0],
 if(program.config) {
   configFile = path.resolve(program.config)
 }
+
+reporter = require(reporterBasePath + program.reporter);
 
 // Spawn process to run PhantomJS from CLI
 // TODO: Is this the way to go?
@@ -51,5 +57,17 @@ function run(cmd, args, callback) {
 }
 
 run("phantomjs", [phantomRunner, basePath, baseVersion, inspectLocation, configFile], function(result) {
-  console.log(result)
+
+  var errors
+
+  try {
+    errors = JSON.parse(result)
+  } catch(error) {
+    console.log(result + '\n')
+  }
+
+  if(errors) {
+    reporter.write(errors)
+  }
+
 })
