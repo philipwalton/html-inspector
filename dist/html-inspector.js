3e3dc194d74d640f22a518ac305258b4debe91f2
Philip Walton <philip@philipwalton.com>
2013-07-29 00:17:23 -0700

    Rerun tests.

--- dist/html-inspector.js
+++ dist/html-inspector.js
@@ -1,12 +1,3 @@
-/*!
- * HTML Inspector - v0.4.1
- *
- * Copyright (c) 2013 Philip Walton <http://philipwalton.com>
- * Released under the MIT license
- *
- * Date: 2013-07-28
- */
-
 (function(e){if("function"==typeof bootstrap)bootstrap("htmlinspector",e);else if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else if("undefined"!=typeof ses){if(!ses.ok())return;ses.makeHTMLInspector=e}else"undefined"!=typeof window?window.HTMLInspector=e():global.HTMLInspector=e()})(function(){var define,ses,bootstrap,module,exports;
 return (function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
 var forEach = require('./forEach');
